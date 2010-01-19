3cf00e9eee45c53bf7218aba862321fbe5ef2f8b
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2010-01-19 13:18:39 +0000

    Proxy: - Refactor BeautifierFilter - Add checkstyle 5.0 jar
    git-svn-id: svn://svn.code.sf.net/p/htmlunit/code/trunk@5367 5f5364db-9458-4db8-a492-e30667be6df6

--- proxy/src/main/java/net/sourceforge/htmlunit/proxy/BeautifierFilter.java
+++ proxy/src/main/java/net/sourceforge/htmlunit/proxy/BeautifierFilter.java
@@ -59,175 +59,55 @@ public class BeautifierFilter implements Filter {
         Parser parser = new Parser(); 
         AstRoot root = parser.parse(source, "<cmd>", 0);
         StringBuilder builder = new StringBuilder();
-        beautify(root, builder, 0);
+        print(root, builder, 0);
         return builder.toString();
     }
 
-    private void beautify(final Node node, final StringBuilder sb, int depth) {
+    private void print(final Node node, final StringBuilder sb, int depth) {
         if (node instanceof AstRoot) {
-            for (Node child : node) {
-                beautify(child, sb, depth);
-            }
+            print((AstRoot) node, sb, depth);
         }
         else if (node instanceof FunctionNode) {
-            FunctionNode f = (FunctionNode) node;
-            makeIndent(depth, sb);
-            sb.append("function");
-            if (f.getFunctionName() != null) {
-                sb.append(" ");
-                beautify(f.getFunctionName(), sb, 0);
-            }
-            if (f.getParams().isEmpty()) {
-                sb.append("() ");
-            } else {
-                sb.append("(");
-                printList(f.getParams(), sb);
-                sb.append(") ");
-            }
-            if (f.isExpressionClosure()) {
-                sb.append(" ");
-                beautify(f.getBody(), sb, 0);
-            } else {
-                int previousLength = sb.length();
-                beautify(f.getBody(), sb, 0);
-                sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
-            }
-            if (f.getFunctionType() == FunctionNode.FUNCTION_STATEMENT) {
-                sb.append("\n");
-            }
+            print((FunctionNode) node, sb, depth);
         }
         else if (node instanceof Name) {
-            Name name = (Name) node;
-            makeIndent(depth, sb);
-            sb.append(name.getIdentifier() == null ? "<null>" : name.getIdentifier());
+            print((Name) node, sb, depth);
         }
         else if (node instanceof Block) {
-            makeIndent(depth, sb);
-            sb.append("{\n");
-            for (Node kid : node) {
-                beautify(kid, sb, depth + 1);
-            }
-            makeIndent(depth, sb);
-            sb.append("}\n");
+            print((Block) node, sb, depth);
         }
         else if (node instanceof IfStatement) {
-            IfStatement s = (IfStatement) node;
-            makeIndent(depth, sb);
-            sb.append("if (");
-            beautify(s.getCondition(), sb, 0);
-            sb.append(") ");
-            if (!(s.getThenPart() instanceof Block)) {
-                sb.append("\n");
-                makeIndent(depth, sb);
-            }
-            int previousLength = sb.length();
-            beautify(s.getThenPart(), sb, depth);
-            sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
-
-            if (s.getElsePart() instanceof IfStatement) {
-                sb.append(" else ");
-                previousLength = sb.length();
-                beautify(s.getElsePart(), sb, depth);
-                sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
-            } else if (s.getElsePart() != null) {
-                sb.append(" else ");
-                previousLength = sb.length();
-                beautify(s.getElsePart(), sb, depth);
-                sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
-            }
-            sb.append("\n");
+            print((IfStatement) node, sb, depth);
         }
         else if (node instanceof UnaryExpression) {
-            UnaryExpression e = (UnaryExpression) node;
-            makeIndent(depth, sb);
-            if (!e.isPostfix()) {
-                sb.append(AstNode.operatorToString(e.getType()));
-                if (e.getType() == Token.TYPEOF || e.getType() == Token.DELPROP) {
-                    sb.append(" ");
-                }
-            }
-            beautify(e.getOperand(), sb, 0);
-            if (e.isPostfix()) {
-                sb.append(AstNode.operatorToString(e.getType()));
-            }
+            print((UnaryExpression) node, sb, depth);
         }
         else if (node instanceof Scope) {
-            makeIndent(depth, sb);
-            sb.append("{\n");
-            for (Node kid : node) {
-                beautify(kid, sb, depth + 1);
-            }
-            makeIndent(depth, sb);
-            sb.append("}\n");
+            print((Scope) node, sb, depth);
         }
         else if(node instanceof PropertyGet) {
-            makeIndent(depth, sb);
-            beautify(((PropertyGet) node).getLeft(), sb, 0);
-            sb.append(".");
-            beautify(((PropertyGet) node).getRight(), sb, 0);
+            print((PropertyGet) node, sb, depth);
         }
         else if (node instanceof TryStatement) {
-            TryStatement s = (TryStatement) node;
-            makeIndent(depth, sb);
-            sb.append("try ");
-            int previousLength = sb.length();
-            beautify(s.getTryBlock(), sb, depth);
-            sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
-
-            for (CatchClause cc : s.getCatchClauses()) {
-                beautify(cc, sb, depth);
-            }
-            if (s.getFinallyBlock() != null) {
-                sb.append(" finally ");
-                beautify(s.getFinallyBlock(), sb, depth);
-            }
+            print((TryStatement) node, sb, depth);
         }
         else if (node instanceof ExpressionStatement) {
-            beautify(((ExpressionStatement) node).getExpression(), sb, depth);
-            sb.append(";\n");
+            print((ExpressionStatement) node, sb, depth);
         }
         else if (node instanceof FunctionCall) {
-            FunctionCall f = (FunctionCall) node;
-            makeIndent(depth, sb);
-            beautify(f.getTarget(), sb, 0);
-            sb.append("(");
-            if (!f.getArguments().isEmpty()) {
-                printList(f.getArguments(), sb);
-            }
-            sb.append(")");
+            print((FunctionCall) node, sb, depth);
         }
         else if (node instanceof CatchClause) {
-            CatchClause c = (CatchClause) node;
-            makeIndent(depth, sb);
-            sb.append("catch (");
-            beautify(c.getVarName(), sb, 0);
-            if (c.getCatchCondition() != null) {
-                sb.append(" if ");
-                beautify(c.getCatchCondition(), sb, 0);
-            }
-            sb.append(") ");
-            beautify(c.getBody(), sb, 0);
+            print((CatchClause) node, sb, depth);
         }
         else if (node instanceof ReturnStatement) {
-            makeIndent(depth, sb);
-            sb.append("return");
-            if (((ReturnStatement) node).getReturnValue() != null) {
-                sb.append(" ");
-                beautify(((ReturnStatement) node).getReturnValue(), sb, 0); 
-            }
-            sb.append(";\n");
-
+            print((ReturnStatement) node, sb, depth);
         }
         else if (node instanceof StringLiteral) {
-            StringLiteral s = (StringLiteral) node;
-            makeIndent(depth, sb);
-            sb.append(s.getQuoteCharacter())
-            .append(ScriptRuntime.escapeString(s.getValue(), s.getQuoteCharacter()))
-            .append(s.getQuoteCharacter());
+            print((StringLiteral) node, sb, depth);
         }
         else if (node instanceof NumberLiteral) {
-            makeIndent(depth, sb);
-            sb.append(((NumberLiteral) node).getValue() == null ? "<null>" : ((NumberLiteral) node).getValue());
+            print((NumberLiteral) node, sb, depth);
         }
         else {
             throw new RuntimeException("Unknown " + node.getClass().getName());
@@ -249,10 +129,182 @@ public class BeautifierFilter implements Filter {
         int max = items.size();
         int count = 0;
         for (AstNode item : items) {
-            beautify(item, sb, 0);
+            print(item, sb, 0);
             if (count++ < max-1) {
                 sb.append(", ");
             }
         }
     }
+
+    protected void print(final AstRoot node, final StringBuilder sb, int depth) {
+        for (final Node child : node) {
+            print(child, sb, depth);
+        }
+    }
+
+    protected void print(final FunctionNode node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("function");
+        if (node.getFunctionName() != null) {
+            sb.append(" ");
+            print(node.getFunctionName(), sb, 0);
+        }
+        if (node.getParams().isEmpty()) {
+            sb.append("() ");
+        } else {
+            sb.append("(");
+            printList(node.getParams(), sb);
+            sb.append(") ");
+        }
+        if (node.isExpressionClosure()) {
+            sb.append(" ");
+            print(node.getBody(), sb, 0);
+        } else {
+            int previousLength = sb.length();
+            print(node.getBody(), sb, 0);
+            sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
+        }
+        if (node.getFunctionType() == FunctionNode.FUNCTION_STATEMENT) {
+            sb.append("\n");
+        }
+    }
+
+    protected void print(final Name node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append(node.getIdentifier() == null ? "<null>" : node.getIdentifier());
+    }
+
+    protected void print(final Block node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("{\n");
+        for (final Node kid : node) {
+            print(kid, sb, depth + 1);
+        }
+        makeIndent(depth, sb);
+        sb.append("}\n");
+    }
+
+    protected void print(final IfStatement node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("if (");
+        print(node.getCondition(), sb, 0);
+        sb.append(") ");
+        if (!(node.getThenPart() instanceof Block)) {
+            sb.append("\n");
+            makeIndent(depth, sb);
+        }
+        int previousLength = sb.length();
+        print(node.getThenPart(), sb, depth);
+        sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
+
+        if (node.getElsePart() instanceof IfStatement) {
+            sb.append(" else ");
+            previousLength = sb.length();
+            print(node.getElsePart(), sb, depth);
+            sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
+        } else if (node.getElsePart() != null) {
+            sb.append(" else ");
+            previousLength = sb.length();
+            print(node.getElsePart(), sb, depth);
+            sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
+        }
+        sb.append("\n");
+    }
+
+    protected void print(final UnaryExpression node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        if (!node.isPostfix()) {
+            sb.append(AstNode.operatorToString(node.getType()));
+            if (node.getType() == Token.TYPEOF || node.getType() == Token.DELPROP) {
+                sb.append(" ");
+            }
+        }
+        print(node.getOperand(), sb, 0);
+        if (node.isPostfix()) {
+            sb.append(AstNode.operatorToString(node.getType()));
+        }
+    }
+
+    protected void print(final Scope node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("{\n");
+        for (Node kid : node) {
+            print(kid, sb, depth + 1);
+        }
+        makeIndent(depth, sb);
+        sb.append("}\n");
+    }
+
+    protected void print(final PropertyGet node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        print(node.getLeft(), sb, 0);
+        sb.append(".");
+        print(node.getRight(), sb, 0);
+    }
+
+    protected void print(final TryStatement node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("try ");
+        int previousLength = sb.length();
+        print(node.getTryBlock(), sb, depth);
+        sb.replace(previousLength, sb.length(), sb.substring(previousLength, sb.length()).trim());
+
+        for (CatchClause cc : node.getCatchClauses()) {
+            print(cc, sb, depth);
+        }
+        if (node.getFinallyBlock() != null) {
+            sb.append(" finally ");
+            print(node.getFinallyBlock(), sb, depth);
+        }
+    }
+
+    protected void print(final ExpressionStatement node, final StringBuilder sb, int depth) {
+        print(node.getExpression(), sb, depth);
+        sb.append(";\n");
+    }
+
+    protected void print(final FunctionCall node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        print(node.getTarget(), sb, 0);
+        sb.append("(");
+        if (!node.getArguments().isEmpty()) {
+            printList(node.getArguments(), sb);
+        }
+        sb.append(")");
+    }
+
+    protected void print(final CatchClause node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("catch (");
+        print(node.getVarName(), sb, 0);
+        if (node.getCatchCondition() != null) {
+            sb.append(" if ");
+            print(node.getCatchCondition(), sb, 0);
+        }
+        sb.append(") ");
+        print(node.getBody(), sb, 0);
+    }
+
+    protected void print(final ReturnStatement node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append("return");
+        if (node.getReturnValue() != null) {
+            sb.append(" ");
+            print(node.getReturnValue(), sb, 0); 
+        }
+        sb.append(";\n");
+    }
+
+    protected void print(final StringLiteral node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append(node.getQuoteCharacter())
+        .append(ScriptRuntime.escapeString(node.getValue(), node.getQuoteCharacter()))
+        .append(node.getQuoteCharacter());
+    }
+
+    protected void print(final NumberLiteral node, final StringBuilder sb, int depth) {
+        makeIndent(depth, sb);
+        sb.append(node.getValue() == null ? "<null>" : node.getValue());
+    }
+
 }
